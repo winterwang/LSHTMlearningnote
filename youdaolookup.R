4e614db499d4b252c5d25eb4ca43f1053216e6d9
winterwang <abelardccwang@gmail.com>
2017-12-23 17:53:30 +0000

    AT4.2.4

--- youdaolookup.R
+++ youdaolookup.R
@@ -3,7 +3,7 @@ library(RCurl)
 library(rjson)
 apikey = "498375134"
 keyfrom = "JustForTestYouDao"
-youdaoLookUp("general principles",api=apikey,keyfrom=keyfrom)
+youdaoLookUp("product moment",api=apikey,keyfrom=keyfrom)
 
 
 
