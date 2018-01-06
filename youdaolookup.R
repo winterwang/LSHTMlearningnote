0d29ed7f4a1f3af8e113e3106942f8536b187b53
winterwang <abelardccwang@gmail.com>
2018-01-06 19:09:21 +0000

    lm 6.9

--- youdaolookup.R
+++ youdaolookup.R
@@ -3,7 +3,7 @@ library(RCurl)
 library(rjson)
 apikey = "498375134"
 keyfrom = "JustForTestYouDao"
-youdaoLookUp("atypically",api=apikey,keyfrom=keyfrom)
+youdaoLookUp("tangent",api=apikey,keyfrom=keyfrom)
 
 
 
