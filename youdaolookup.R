43df14e9693bd3961edaec9f6a48dc23c3ddffc7
winterwang <abelardccwang@gmail.com>
2018-01-08 23:27:27 +0000

    lm7.7

--- youdaolookup.R
+++ youdaolookup.R
@@ -3,7 +3,7 @@ library(RCurl)
 library(rjson)
 apikey = "498375134"
 keyfrom = "JustForTestYouDao"
-youdaoLookUp("leverage",api=apikey,keyfrom=keyfrom)
+youdaoLookUp("interpretation",api=apikey,keyfrom=keyfrom)
 
 
 
