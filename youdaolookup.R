d172982822335ffdc353c28f472a6c084211b014
winterwang <abelardccwang@gmail.com>
2018-01-15 23:35:24 +0000

    glm 4-40

--- youdaolookup.R
+++ youdaolookup.R
@@ -3,7 +3,7 @@ library(RCurl)
 library(rjson)
 apikey = "498375134"
 keyfrom = "JustForTestYouDao"
-youdaoLookUp("nested model",api=apikey,keyfrom=keyfrom)
+youdaoLookUp("scaled",api=apikey,keyfrom=keyfrom)
 
 
 
