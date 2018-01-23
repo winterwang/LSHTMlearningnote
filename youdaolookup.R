6b85bea4d4431716c6d8ba52a97d1be6f43eb0e9
winterwang <abelardccwang@gmail.com>
2018-01-23 00:56:47 +0000

    GLM 8-81

--- youdaolookup.R
+++ youdaolookup.R
@@ -3,7 +3,7 @@ library(RCurl)
 library(rjson)
 apikey = "498375134"
 keyfrom = "JustForTestYouDao"
-youdaoLookUp("benzine",api=apikey,keyfrom=keyfrom)
+youdaoLookUp("prevalence",api=apikey,keyfrom=keyfrom)
 
 
 
