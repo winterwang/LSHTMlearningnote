04a89f05ae68e0569638af92e1e785202de0015c
winterwang <abelardccwang@gmail.com>
2018-01-24 18:26:38 +0000

    GLM 9-92

--- youdaolookup.R
+++ youdaolookup.R
@@ -3,7 +3,7 @@ library(RCurl)
 library(rjson)
 apikey = "498375134"
 keyfrom = "JustForTestYouDao"
-youdaoLookUp("cubersome",api=apikey,keyfrom=keyfrom)
+youdaoLookUp("expectation",api=apikey,keyfrom=keyfrom)
 
 
 
