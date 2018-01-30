a0785f13ab01f68072d9a3f0be65313f45d1c68a
winterwang <abelardccwang@gmail.com>
2018-01-30 01:13:33 +0000

    GLM 11 done

--- youdaolookup.R
+++ youdaolookup.R
@@ -3,7 +3,7 @@ library(RCurl)
 library(rjson)
 apikey = "498375134"
 keyfrom = "JustForTestYouDao"
-youdaoLookUp("variation",api=apikey,keyfrom=keyfrom)
-
+youdaoLookUp("percentile",api=apikey,keyfrom=keyfrom)
+  
 
 
