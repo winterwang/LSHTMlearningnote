baaa9e61d20b96e66bd10e7f959c2eec9dfb548e
winterwang <abelardccwang@gmail.com>
2018-02-21 12:38:50 +0000

    update for all caches

--- youdaolookup.R
+++ youdaolookup.R
@@ -3,6 +3,6 @@ library(RCurl)
 library(rjson)
 apikey = "498375134"
 keyfrom = "JustForTestYouDao"
-youdaoLookUp("hierarchical",api=apikey,keyfrom=keyfrom)
+youdaoLookUp("Pseudoreplication",api=apikey,keyfrom=keyfrom)
 
 
